bdf89297fe1ed62f5850d7fc6bf6e16986c3456a
Enzo Teixeira Alves <enzoteixeira2022@gmail.com>
2026-07-27 17:00:52 -0300

    chore: initialize TracePass backend

---
+++ apps/tracepass/backend/tracepass-api/src/main/java/TracePass/API/TracepassApiApplication.java
@@ -0,0 +1,13 @@
+package TracePass.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracepassApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracepassApiApplication.class, args);
+	}
+
+}
