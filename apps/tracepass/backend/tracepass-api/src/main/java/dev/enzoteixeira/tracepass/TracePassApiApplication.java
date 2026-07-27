e3e3fefcf644f7e0a55aaf7bb9fccd86bbfaa3f3
Enzo Teixeira Alves <enzoteixeira2022@gmail.com>
2026-07-27 17:18:47 -0300

    refactor: standardize TracePass Java packages

---
+++ apps/tracepass/backend/tracepass-api/src/main/java/dev/enzoteixeira/tracepass/TracePassApiApplication.java
@@ -0,0 +1,12 @@
+package dev.enzoteixeira.tracepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracePassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracePassApiApplication.class, args);
+    }
+}
---
+++ apps/tracepass/backend/tracepass-api/src/test/java/dev/enzoteixeira/tracepass/TracePassApiApplicationTests.java.java
@@ -0,0 +1,12 @@
+package dev.enzoteixeira.tracepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracePassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
